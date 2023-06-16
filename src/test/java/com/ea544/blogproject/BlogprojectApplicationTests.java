880aa5595bdc56b37c4f744081766d366a26c0ba
mohammed <mohammedrashaideh23@gmail.com>
2023-06-15 21:32:42 -0500

    initial commit

---
+++ src/test/java/com/ea544/blogproject/BlogprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ea544.blogproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
